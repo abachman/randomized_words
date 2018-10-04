9c68b1454e76039ebd40bf06f1fad6705ee6c8b9
Adam Bachman <adam.bachman@gmail.com>
2018-10-04 18:23:26 -0400

    prettier inspect for Words class

--- lib/randomized_words/words.rb
+++ lib/randomized_words/words.rb
@@ -2,13 +2,20 @@ require 'randomized_words/markov'
 
 module RandomizedWords
   class Words
+    attr_reader :words, :letter_count, :min_length, :max_length
+
     def initialize(words=nil, letter_count=nil, min_length=nil, max_length=nil)
-      if words.nil?
+      @words = words
+      @letter_count = letter_count
+      @min_length = min_length
+      @max_length = max_length
+
+      if @words.nil?
         path = File.join File.dirname(__FILE__), 'data', 'latin_words.txt'
-        words = self.class.parse_file(path)
+        @words = self.class.parse_file(path)
       end
 
-      @markov = Markov.new(words: words, letter_count: letter_count, min_length: min_length, max_length: max_length)
+      @markov = Markov.new(words: @words, letter_count: @letter_count, min_length: @min_length, max_length: @max_length)
     end
 
     def self.parse_file(word_file_path)
@@ -23,7 +30,7 @@ module RandomizedWords
     end
 
     def to_s
-      "<RandomizedWords::Words #{self.object_id}>"
+      "<RandomizedWords::Words words=(#{words.size} items) letter_count=#{letter_count.inspect} min_length=#{min_length.inspect} max_length=#{max_length.inspect}>"
     end
 
     def inspect
